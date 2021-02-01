7e615ecfce014d52110f140871f6d0d6840d232f
Chiara <chiara.gambardella94@gmail.com>
2021-02-01 19:47:25 +0100

    check login, select guests, add check login js in form event

--- Event.js
+++ Event.js
@@ -1,22 +1,57 @@
-//creare un input selezione tramire CreateElement("")collegandolo all'html
-// al click-->(addEventListener())parte la fetch
-//dove l'utente puo scegliere gli invitati, tramite fetch
-//cioè tutti quelli che si sono iscritti all'app
-const response = [
-  { id: 1, name: "Ani" },
-  { id: 2, name: "Eni" },
-  { id: 3, name: "Fulvio" },
-];
+const form_event = document.querySelector("#form_event");
+//creare una fetch per richiamare i dati dell'utente in corso
 
-const select = document.querySelector("#guest");
+// prendiamo il token identificativo per farci riconoscere dal server (il getItem vuole solo la chiave)
+const current_token = localStorage.getItem("access_token");
 
-for (const i of response) {
-  const options = document.createElement("option");
-  options.text = i.name;
-  select.options.add(options);
+async function getMe() {
+  const me_response = await fetch("https://iopasso.kopiro.me/me", {
+    method: "GET",
+    headers: {
+      authorization: "Bearer " + current_token,
+    },
+  });
+  const me_json = await me_response.json();
+  const user_online = me_json.name;
+  console.log(user_online);
+  return user_online;
 }
-//add eventListner
-//fetch
-//ciclare la response del server e prenderci solo i valori di name
 
-//create a html option tag for chose the guests
+getMe();
+
+//creo asyc funnction per prendere con la fetch in get la lista di tutti gli utenti
+async function listGuests() {
+  const guests_response = await fetch("https://iopasso.kopiro.me/guests", {
+    method: "GET",
+    headers: {
+      authorization: "Bearer " + current_token,
+    },
+  });
+  // trasformiamo la lista in json
+  const guest_json = await guests_response.json();
+
+  //prendiamo l'elemento select dall´html
+  const select = document.querySelector("#guest");
+  //cicliamo la risposta della fetch in get per prenderci solo il nome dell'utente
+  for (const i of guest_json) {
+    //abbiamo creato l'elemento html option del tag select in javascript
+    const options = document.createElement("option");
+    options.text = i.name;
+    //abbiamo aggiunto i nomi degli utenti come option del select
+    select.options.add(options);
+  }
+}
+//richiamiamo la funzione
+listGuests();
+
+form_event.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form_event);
+
+  const select = document.querySelector("#guest");
+
+  const response = await fetch("https://iopasso.kopiro.me/form_event", {
+    method: "POST",
+    body: formData,
+  });
+});
