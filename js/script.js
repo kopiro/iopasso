b2c2924a314e0b0987f87c771527eccc48397b16
Carmela Pippa <carmelapippa@gmail.com>
2021-02-26 19:20:35 +0100

    Insert logo, modifica barra menù, modifica di index, fetch getme in scipt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 const container = document.querySelector("#container");
 const current_token = localStorage.getItem("access_token");
+
+async function getMe() {
+  const response = await fetch(`${window.API_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      authorization: "Bearer " + current_token,
+    },
+  });
+  const me = await response.json();
+  const user_online = me.name;
+  console.log(user_online);
+  return user_online;
+}4
 //ci prendiamo la lista degli eventi creati fin ora da tutti gli utenti
 async function getListOfEvents() {
   const response = await fetch(`${window.API_URL}/events?limit=2&page=0`, {
